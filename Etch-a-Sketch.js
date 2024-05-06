c48f2f26f67ce81b0dddca2f3352e8fa48a746c9
alanzhang03 <alan.s.zhang@gmail.com>
2024-05-05 20:47:53 -0400

    using grid instead

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -2,12 +2,11 @@ const gridContainer = document.querySelector("#grid-container");
 const totalRows = 16;
 const totalCols = 16;
 
-for(let row=0; i < totalRows; row++){
-    for(let col=0; j < totalCols; col++){
+for(let row=0; row < totalRows; row++){
+    for(let col=0; col < totalCols; col++){
         let grid = document.createElement("div");
         grid.classList.add("grid-item");
         gridContainer.appendChild(grid);
     }
-
 }
 
