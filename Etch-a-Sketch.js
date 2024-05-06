72e3ceccd6667fe4d17864c0dd0a83f388c19785
alanzhang03 <alan.s.zhang@gmail.com>
2024-05-05 21:41:34 -0400

    buttons

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,5 +1,5 @@
 const gridContainer = document.querySelector("#grid-container");
-
+const buttons = document.querySelector('.buttons');
 
 for(let i = 0; i < 256; i++){
     gridItem = document.createElement("grid-item");
