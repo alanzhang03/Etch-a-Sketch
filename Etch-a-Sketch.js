c63762ba8514e21bb752f1a00f676f3d0c3d6197
alanzhang03 <alan.s.zhang@gmail.com>
2024-05-07 00:15:37 -0400

    done a couple event handlers

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -2,32 +2,47 @@ const gridContainer = document.querySelector("#grid-container");
 const buttons = document.querySelector('#buttons-container');
 let currentColor = '#000000';
 
+function buildGridItems(){
+    for(let i = 0; i < 256; i++){
+        grids = document.createElement("div");
+        grids.className = 'grid-item';
+        gridContainer.appendChild(grids);
 
-for(let i = 0; i < 256; i++){
-    grids = document.createElement("grid-item");
-    grids.className = 'grid-item';
-    gridContainer.appendChild(grids);
+        grids.addEventListener('mouseover', function(){
+            this.style.backgroundColor = currentColor;
+        });
 
-
-
-
+    }
 }
+
+buildGridItems();
+
+const colorPickingButton = document.createElement("input");
+colorPickingButton.textContent = "Color: ";
+colorPickingButton.type = "color";
+colorPickingButton.id = "colorPicker";
+colorPickingButton.value = "#000000";
+colorPickingButton.className = "buttons";
+
+buttons.appendChild(colorPickingButton);
+
+colorPickingButton.addEventListener("input", function(){
+    console.log("Color chosen:", this.value);
+    currentColor = this.value;
+})
+
 
 const clearButton = document.createElement("button");
 clearButton.className = 'buttons';
 clearButton.textContent = "clear button!";
 buttons.appendChild(clearButton);
 
-
-const colorPickingButton = document.createElement("button");
-colorPickingButton.className = "buttons";
-colorPickingButton.textContent = "pick button!";
-buttons.appendChild(colorPickingButton);
-
-
-
-
-
+clearButton.addEventListener("click", function(){
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    buildGridItems();
+})
 
 
 const eraseButton = document.createElement("button");
